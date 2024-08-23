8bb4bf5dc0cb12d219c6a152cdb6ba0cfed0e700
Dorian Marié <dorian@dorianmarie.com>
2024-08-23 23:54:32 +0200

    remove extra newline and correct rspec describe block

--- spec/json_read_write_spec.rb
+++ spec/json_read_write_spec.rb
@@ -2,5 +2,12 @@
 
 require "spec_helper"
 
-RSpec.describe "each" do
+RSpec.describe "json-read-write" do
+  it "works" do
+    expect(
+      `cat package.json | bin/json-read-write`
+    ).to eq(
+      File.read("package.json")
+    )
+  end
 end
