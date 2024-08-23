aee2d8b6a07c72b930221af1cf97279aa85e6422
Dorian Marié <dorian@dorianmarie.com>
2024-08-24 00:44:01 +0200

    update dorian-arguments gem and clean up code formatting

--- spec/json_read_write_spec.rb
+++ spec/json_read_write_spec.rb
@@ -4,9 +4,7 @@ require "spec_helper"
 
 RSpec.describe "json-read-write" do
   it "works" do
-    expect(
-      `cat package.json | bin/json-read-write`
-    ).to eq(
+    expect(`cat package.json | bin/json-read-write`).to eq(
       File.read("package.json")
     )
   end
